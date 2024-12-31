ce5d72181decd150fb5e72fdc519503e4cbf8cf5
JKY086-秦鑫 <qinxin@wondersgroup.com>
2024-12-31 13:46:27 +0800

    初始化

---
+++ build/CMakeFiles/outputppm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/outputppm.dir/ppmOutput.cpp.obj"
+  "CMakeFiles/outputppm.dir/ppmOutput.cpp.obj.d"
+  "liboutputppm.dll.a"
+  "outputppm.exe"
+  "outputppm.exe.manifest"
+  "outputppm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/outputppm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/outputppm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for outputppm.
+# This may be replaced when dependencies are built.
